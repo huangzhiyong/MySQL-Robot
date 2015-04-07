e3cdf1270f9cc57a4dd4c817e5f2e5af39106e1b
huangzhiyong <huangzhiyong@e-future.com.cn>
2015-04-07 17:32:31 +0800

    add func read contents from file

---
+++ common/common.go
@@ -0,0 +1,35 @@
+package common
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func ReadLinesOffset(filename string, offset int, n int) ([]string, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return []string{""}, err
+	}
+	defer f.Close()
+
+	var res []string
+
+	r := bufio.NewReader(f)
+	for i := 0; i < n+offset || n < 0; i++ {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			break
+		}
+		if i < offset {
+			continue
+		}
+
+		res = append(res, strings.Trim(line, "\n"))
+	}
+	return ret, nil
+}
+
+func ReadLinesAll(filename string) ([]string, error) {
+	return ReadLinesOffset(filename, 0, -1)
+}
